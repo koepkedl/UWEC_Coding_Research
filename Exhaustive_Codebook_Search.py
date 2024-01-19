a5917eb9efa0b619c93ec4473a60a4575a0b3631
Eric Canton <eric.d.canton@gmail.com>
2024-01-19 14:46:15 -0600

    clean up code, make comments

--- Exhaustive_Codebook_Search.py
+++ Exhaustive_Codebook_Search.py
@@ -11,15 +11,15 @@
 
 import numpy as np
 import math
-from tqdm import tqdm
 from itertools import chain, combinations, product
 import time
 
-def binomialcoef(j,k):
-    j = int(j)
-    k = int(k)
-    return math.factorial(j)/(math.factorial(k)*math.factorial(j-k))
+# this gives us a progress bar on for loops automagically
+from tqdm import tqdm
 
+# A class that keeps track of elapsed time for us, and how many times we've checked the time
+# Then you can just add a line like "timer.print_elapsed_time()" to your code to see how long it's been running,
+# and note how many times you've called this function before now. Probably better ways, but this is easy.
 class TimeChecker:
     def __init__(self, places_to_print: int = 3):
         self.time_check_count = 0
@@ -38,12 +38,29 @@ class TimeChecker:
     def elapsed_time(self) -> float:
         return self.normalize * round(self.scale_up * (time.time() - self.start_time))
 
-# TaiChi code
-n = 5 #This sets the block length.
-import taichi as ti
-ti.init(arch=ti.gpu)
+# powerset makes sets of sizes from 2 until the width of the poset. 
+# make width a parameter, better than having it bound to the global variable width below
+def powerset(iterable, width):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(2,width+1))
 
-powerset_ti = ti.field(shape=(2**n, n), dtype=ti.i32)
+
+
+# unique returns a list containing the unique values of the input list.
+def unique(list1):
+    list_set = set(list1)
+    unique_list = list(list_set)
+    return unique_list
+
+# binomialcoef generates the binomial coefficient n choose k.
+def binomialcoef(j,k):
+    j = int(j)
+    k = int(k)
+    return math.factorial(j)/(math.factorial(k)*math.factorial(j-k))
+
+
+n = 5 #This sets the block length.
+
 codewords = []
 adversary_words = []
 
@@ -53,86 +70,53 @@ for z in range(2**n):
     codewords.append(int(np.binary_repr(z)))
     adversary_words.append(int(np.binary_repr(z)))
 
-@ti.kernel
-def fill_powerset(iterable, width):
-    # s = list(iterable)
-    return chain.from_iterable(combinations(iterable, r) for r in range(2,width+1))
-
-# powerset makes sets of sizes from 2 until the width of the poset. 
-
-
-# binomialcoef generates the binomial coefficient n choose k.
-
-def unique(list1):
-    list_set = set(list1)
-    unique_list = list(list_set)
-    return unique_list
-
-# unique returns a list containing the unique values of the input list.
 
 timer = TimeChecker()
 
 
-# 0
+# Since this is the first time we called .print_elapsed_time(), the counter is at (0)
+# (0) --- #### seconds ---
 timer.print_elapsed_time()
 
 codewords.pop(2**n-1)
 codewords.pop(0)
-# codewords_idx = {x: i for i, x in enumerate(codewords)}
 adversary_words.pop(0)
 
-# 1
+# (1)
 timer.print_elapsed_time()
 
 codebooks = []
-# codebooks = list(map(lambda c: set(c), powerset(codewords, width)))
-codebooks = list(fill_powerset(codewords, width))
+codebooks = list(powerset(codewords, width))
 codebookpairs = []
 
 # 2
 timer.print_elapsed_time()
 
-# 2D field of 8 bit integers (just inclusion) 
-codebookpairs_ti = ti.field(shape=(len(codebooks), 2, len(codewords)), dtype=ti.i32)
-codebookpairs_ti.fill(0)
-
-for i in range(len(codebooks)):
-    for x in range(i+1,len(codebooks)):
-        codebookpairs_ti[i, 0]
-        # if len(codebooks[i]) + len(codebooks[x]) == len(set(codebooks[i] + codebooks[x])):
-        #     for c in codebooks[i]:
-        #         codebookpairs_ti[i, codewords_idx[c]] = 1
-        #     for c in codebooks[x]:
-        #         codebookpairs_ti[x, codewords_idx[c]] = 1
-    
-timer.print_elapsed_time()
-
-
-a = []
+# this is called a "list comprehension"
+# They can be quite a bit faster than .append in a for loop
+a = [j * j for j in range(5)]
+b = []
 for j in range(5):
-    a.append(j * j)
-
-b = [j * j for j in range(5) if j % 2 == 0]
-
+    b.append(j * j)
+a == b
 
 
-
-
-i = 0
-# Question: can we generate this faster using a partition of the set of codewords?
-# EG remove this quadtratic loop, instead iterate on partitions of subsets of the set of codewords.
-# and split these as we iterate, instead of iterating and checking for empty intersection.
-# changing to list comprehension saves 33% of time
-# the if statement at the end is another 33% savings
-# STILL NOT ENOUGH for n=5, which has 53009071 iterations on the outer loop
-codebookpairs = [
+# compute pairs [A, B] of subsets of `codewords` such that A and B are disjoint
+# where |A| and |B| are between 2 and width 
+codebookpairs = [ 
     [codebooks[i], codebooks[x]]
     for i in tqdm(range(len(codebooks)))
     for x in range(i+1,len(codebooks))
     
+    ## it's faster to check it this way than in the commented out way
+    ## but this is still O(N^2), N = len(codebooks) since pairwise-check
+    ##   > Can we instead generate subsets C of `codewords` and 
+    ##   > then here expand to all ways of writing C = A U B, A n B = 0?
     if len(codebooks[i]) + len(codebooks[x]) == len(set(codebooks[i] + codebooks[x]))
+    ## This check happens to be 33% slower, but it's still O(N^2)
     # if len(set(codebooks[i]).intersection(set(codebooks[x]))) == 0
 ]
+# i = 0
 # while len(codebooks) > i:
 #     print(i, "//", len(codebooks))
 #     for x in range(i+1,len(codebooks)):
@@ -144,17 +128,8 @@ codebookpairs = [
 #     print(len(pairs))
 #     i = i+1
 
-## ERIC'S CODE
-# ones = [x for x in codebookpairs if x[0][0] == 1]
-# from collections import defaultdict
-# s = defaultdict(list)
-# for one in ones:
-#     s[one[0][1]].append(one[1])
-## END ERIC'S CODE
-
 # 3
 timer.print_elapsed_time()
-
     
 adversarial_sums = []
 potential_sums = []
@@ -208,10 +183,10 @@ print(len(partially_correctable_codebooks))
 print("Codebooks that are partially correctable for block length "+ str(n))
 # print(partially_correctable_codebooks)
 
-# import pandas as pd
+import pandas as pd
 
-# df = pd.DataFrame(partially_correctable_codebooks)
-# df.to_csv('Good_Codebooks.csv', index=True)
+df = pd.DataFrame(partially_correctable_codebooks)
+df.to_csv('Good_Codebooks.csv', index=True)
 
 
 # In[4]:
